c8a9cfd98fb9c3e3320ab109691d5b92e19fdb36
hahunavth <vuthanhha.2001@gmail.com>
2022-12-19 20:51:53 +0700

    refactor(To typescript): :recycle:

---
+++ src/api/index.ts
@@ -0,0 +1,3 @@
+import axios from "axios";
+
+export const BASE_URL = "https://sp-17-production.fly.dev/api/v1/supplier";
---
+++ src/scenes/dashboard/index.tsx
@@ -0,0 +1,270 @@
+import { Box, Button, IconButton, Typography, useTheme } from "@mui/material";
+import { tokens } from "../../theme";
+import DownloadOutlinedIcon from "@mui/icons-material/DownloadOutlined";
+import LooksOneIcon from "@mui/icons-material/LooksOne";
+import LooksTwoIcon from "@mui/icons-material/LooksTwo";
+import Looks3Icon from "@mui/icons-material/Looks3";
+import Looks4Icon from "@mui/icons-material/Looks4";
+import Header from "../../components/Header";
+import LineChart from "../../components/LineChart";
+import StatBox from "../../components/StatBox";
+import { mockDataTeam } from "../../data/mockData";
+import { mockDataInvoices } from "../../data/mockData";
+
+const Dashboard = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
+  return (
+    <Box m="20px">
+      {/* HEADER */}
+      <Box display="flex" justifyContent="space-between" alignItems="center">
+        <Header title="DASHBOARD" subtitle="Welcome to your dashboard" />
+
+        <Box>
+          <Button
+            sx={{
+              backgroundColor: colors.blueAccent[700],
+              color: colors.grey[100],
+              fontSize: "14px",
+              fontWeight: "bold",
+              padding: "10px 20px",
+            }}
+          >
+            <DownloadOutlinedIcon sx={{ mr: "10px" }} />
+            Download Reports
+          </Button>
+        </Box>
+      </Box>
+
+      <Box>
+        <Typography
+          variant="h5"
+          fontWeight="600"
+          color={colors.grey[100]}
+          marginBottom="20px"
+        >
+          Những sản phẩm nhập nhiều nhất:
+        </Typography>
+      </Box>
+
+      {/* GRID & CHARTS */}
+      <Box
+        display="grid"
+        gridTemplateColumns="repeat(12, 1fr)"
+        gridAutoRows="140px"
+        gap="20px"
+      >
+        {/* ROW 1 */}
+
+        <Box
+          gridColumn="span 3"
+          bgcolor={colors.primary[400]}
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <StatBox
+            price="30.100.000 đ"
+            title="Iphone 13 pro max"
+            progress="0.75"
+            increase="+14%"
+            icon={
+              <LooksOneIcon
+                sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
+              />
+            }
+          />
+        </Box>
+        <Box
+          gridColumn="span 3"
+          bgcolor={colors.primary[400]}
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <StatBox
+            price="80.000 đ"
+            title="Minna no Nihongo 1"
+            progress="0.50"
+            increase="+21%"
+            icon={
+              <LooksTwoIcon
+                sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
+              />
+            }
+          />
+        </Box>
+        <Box
+          gridColumn="span 3"
+          bgcolor={colors.primary[400]}
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <StatBox
+            price="500.000 đ"
+            title="Varsity SP-17"
+            progress="0.30"
+            increase="+5%"
+            icon={
+              <Looks3Icon
+                sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
+              />
+            }
+          />
+        </Box>
+        <Box
+          gridColumn="span 3"
+          bgcolor={colors.primary[400]}
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <StatBox
+            price="450.000 đ"
+            title="Sữa rửa mặt svr 450ml"
+            progress="0.80"
+            increase="+43%"
+            icon={
+              <Looks4Icon
+                sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
+              />
+            }
+          />
+        </Box>
+
+        {/* ROW 2 */}
+        <Box
+          gridColumn="span 12"
+          gridRow="span 2"
+          bgcolor={colors.primary[400]}
+        >
+          <Box
+            mt="25px"
+            p="0 30px"
+            display="flex "
+            justifyContent="space-between"
+            alignItems="center"
+          >
+            <Box>
+              <Typography
+                variant="h5"
+                fontWeight="600"
+                color={colors.grey[100]}
+              >
+                Số lượng hàng nhập
+              </Typography>
+            </Box>
+            <Box>
+              <IconButton>
+                <DownloadOutlinedIcon
+                  sx={{ fontSize: "26px", color: colors.greenAccent[500] }}
+                />
+              </IconButton>
+            </Box>
+          </Box>
+          <Box height="250px" m="-20px 0 0 0">
+            <LineChart isDashboard={true} />
+          </Box>
+        </Box>
+
+        {/* ROW 3 */}
+        {/* Ncc */}
+        <Box
+          gridColumn="span 6"
+          gridRow="span 2"
+          bgcolor={colors.primary[400]}
+          overflow="auto"
+        >
+          <Box
+            display="flex"
+            justifyContent="space-between"
+            alignItems="center"
+            borderBottom={`4px solid ${colors.primary[500]}`}
+            color={colors.grey[100]}
+            p="15px"
+          >
+            <Typography color={colors.grey[100]} variant="h5" fontWeight="600">
+              Danh sách nhà cung cấp
+            </Typography>
+          </Box>
+          {mockDataTeam.map((supplier: any, i: any) => (
+            <Box
+              key={`${supplier.id}-${i}`}
+              display="flex"
+              justifyContent="space-between"
+              alignItems="center"
+              borderBottom={`4px solid ${colors.primary[500]}`}
+              p="15px"
+            >
+              <Box>
+                <Typography
+                  color={colors.greenAccent[500]}
+                  variant="h5"
+                  fontWeight="600"
+                >
+                  {supplier.id}
+                </Typography>
+                <Typography color={colors.grey[100]}>
+                  {supplier.name}
+                </Typography>
+              </Box>
+              <Box color={colors.grey[100]} justifyItems="left">
+                {supplier.email}
+              </Box>
+              <Box color={colors.grey[100]}>{supplier.count}</Box>
+            </Box>
+          ))}
+        </Box>
+        {/* sp */}
+        <Box
+          gridColumn="span 6"
+          gridRow="span 2"
+          bgcolor={colors.primary[400]}
+          overflow="auto"
+        >
+          <Box
+            display="flex"
+            justifyContent="space-between"
+            alignItems="center"
+            borderBottom={`4px solid ${colors.primary[500]}`}
+            color={colors.grey[100]}
+            p="15px"
+          >
+            <Typography color={colors.grey[100]} variant="h5" fontWeight="600">
+              Danh sách sản phẩm
+            </Typography>
+          </Box>
+          {mockDataInvoices.map((product, i) => (
+            <Box
+              key={`${product.id}-${i}`}
+              display="flex"
+              justifyContent="space-between"
+              alignItems="center"
+              borderBottom={`4px solid ${colors.primary[500]}`}
+              p="15px"
+            >
+              <Box>
+                <Typography
+                  color={colors.greenAccent[500]}
+                  variant="h5"
+                  fontWeight="600"
+                >
+                  {product.id}
+                </Typography>
+                <Typography color={colors.grey[100]}>{product.name}</Typography>
+              </Box>
+              <Box color={colors.grey[100]} justifyItems="left">
+                {product.cost}
+              </Box>
+              <Box color={colors.grey[100]}>{product.count}</Box>
+            </Box>
+          ))}
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default Dashboard;
