a03d30b01151486620e488e0c47cf01cef626625
dinh2411 <dinhyt2001@gmail.com>
2022-12-31 22:07:42 +0700

    day la form create new supplier vs show supplierlist vs supplierdetailinfo nhung toi chua them cai nut xoa vs edit vao

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import { Routes, Route } from "react-router-dom";
 import Topbar from "./scenes/global/Topbar";
 import Sidebar from "./scenes/global/Sidebar";
 import Dashboard from "./scenes/dashboard";
-import Team from "./scenes/team";
-import Form from "./scenes/form";
+import Team from "./scenes/SupplierList";
+import Form from "./scenes/createNewSupplierForm";
 // import Invoices from './scenes/invoices';
 // import Contacts from './scenes/contacts';
 // import Bar from './scenes/bar';
@@ -31,8 +31,8 @@ function App() {
             {/* <Topbar setIsSidebar={setIsSidebar} /> */}
             <Routes>
               <Route path="/" element={<Dashboard />} />
-              <Route path="/team" element={<Team />} />
-              <Route path="/form" element={<Form />} />
+              <Route path="/supplierlist" element={<Team />} />
+              <Route path="/createNewSupplierForm" element={<Form />} />
               {/* <Route path="/contacts" element={<Contacts />} />
               <Route path="/invoices" element={<Invoices />} />
               <Route path="/form" element={<Form />} />
