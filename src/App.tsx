a0c326503bd4a70220a92124925a5191723c7acd
hahunavth <vuthanhha.2001@gmail.com>
2022-12-31 23:55:42 +0700

    Fix: conflict

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,8 @@ import Dashboard from "./scenes/dashboard";
 
 import SupplierList from "./scenes/SupplierList";
 import Form from "./scenes/createNewSupplierForm";
-import { CssBaseline, ThemeProvider } from "@mui/material";
-import { ColorModeContext, useMode } from "./theme";
 import SupplierDetailInfo from "./scenes/SupplierDetailInfo";
 import BaoGia from "./scenes/BaoGia";
-import Team from "./scenes/team";
 import ImportStoryList from "./scenes/list/ImportHistoryList";
 import ImportProductsForm from "./scenes/form/ImportProductForm";
 import Invoices from "./scenes/list/index";
@@ -39,28 +36,28 @@ function App() {
               <Topbar />
               <Routes>
                 <Route path="/" element={<Dashboard />} />
-                <Route path="/team" element={<Team />} />
-                <Route path="/supplierlist" element={<SupplierList />} />
-              <Route path="/createNewSupplierForm" element={<Form />} />
-              <Route path="/SupplierDetailInfo" element={<SupplierDetailInfo />} />
-              <Route path="/BaoGia" element={<BaoGia />} />
+                {/* <Route path="/team" element={<Team />} /> */}
+                <Route path="/suppliers" element={<SupplierList />} />
+                <Route path="/suppliers/create" element={<Form />} />
+                <Route path="/suppliers/:id" element={<SupplierDetailInfo />} />
+                {/* <Route path="/BaoGia" element={<BaoGia />} /> */}
                 <Route
-                  path="/importProductsForm"
+                  path="/imports/create"
                   element={<ImportProductsForm />}
                 />
-                <Route path="/importStoryList" element={<ImportStoryList />} />
-                <Route path="/invoices" element={<Invoices />} />
+                <Route path="/imports/history" element={<ImportStoryList />} />
+                {/* <Route path="/invoices" element={<Invoices />} /> */}
                 {/* Price quotation */}
                 <Route
-                  path="/price-quotation/create"
+                  path="/price-quotations/create"
                   element={<CreatePriceQuotation />}
                 />
                 <Route
-                  path="/import/:importRequestId/price-quotation/create"
+                  path="/imports/:importRequestId/price-quotation/create"
                   element={<CreatePriceQuotation />}
                 />
                 <Route
-                  path="/import/:importRequestId/price-quotation-list"
+                  path="/imports/:importRequestId/price-quotation-list"
                   element={<PriceQuotationList />}
                 />
               </Routes>
