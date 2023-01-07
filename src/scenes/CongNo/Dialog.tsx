08ae1f1944fc30ca37117d14079f77d041de0448
Sonle <leducson007@gmail.com>
2023-01-07 22:55:49 +0700

    COng no

---
+++ src/scenes/CongNo/Dialog.tsx
@@ -0,0 +1,52 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import { styled } from '@mui/material/styles';
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+import DialogActions from '@mui/material/DialogActions';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
+import Typography from '@mui/material/Typography';
+
+import { useTheme } from "@mui/material";
+import { tokens } from "../../theme";
+export const BootstrapDialog = styled(Dialog)(({ theme }) => ({
+    '& .MuiDialogContent-root': {
+        padding: theme.spacing(2),
+    },
+    '& .MuiDialogActions-root': {
+        padding: theme.spacing(1),
+    },
+}));
+
+export interface DialogTitleProps {
+    id: string;
+    children?: React.ReactNode;
+    onClose: () => void;
+};
+export function BootstrapDialogTitle(props: DialogTitleProps) {
+    const { children, onClose, ...other } = props;
+
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+    return (
+        <DialogTitle sx={{ m: 0, p: 2 }} {...other}>
+            {children}
+            {onClose ? (
+                <IconButton
+                    aria-label="close"
+                    onClick={onClose}
+                    sx={{
+                        position: 'absolute',
+                        right: 8,
+                        top: 8,
+                        color: colors.greenAccent[300],
+                    }}
+                >
+                    <CloseIcon />
+                </IconButton>
+            ) : null}
+        </DialogTitle>
+    );
+}
--- src/scenes/CongNo/index.tsx
+++ src/scenes/CongNo/index.tsx
@@ -1,8 +1,155 @@
-import React from 'react'
+import * as React from 'react';
+import { Box, Button, Typography, useTheme } from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
+import { tokens } from "../../theme";
+import Header from "../../components/Header";
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import { mockDataReceivable, mockDataCollected } from "./data";
+import DialogContent from '@mui/material/DialogContent';
+import DialogActions from '@mui/material/DialogActions';
+import { BootstrapDialog, BootstrapDialogTitle, DialogTitleProps } from "./Dialog";
 
 const CongNo = () => {
+  const clickHandler = (v: any) => {
+    setContent(v.row);
+    setOpen(true)
+  }
+  const closeHandler = () => {
+    setOpen(false)
+  }
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  let countRow = 0;
+  let countRow1 = 0;
+  const [open, setOpen] = React.useState(false);
+  const [content, setContent] = React.useState('');
+  const columns = [
+    { field: "stt", headerName: "STT", flex: 0.2 },
+    { field: "id", headerName: "ID Hóa đơn", flex: 1 },
+    { field: "date", headerName: "Ngày thu dự kiến", flex: 1 },
+    { field: "price", headerName: "Số tiền", flex: 1 },
+    {
+      field: "info", headerName: "Chi tiết", flex: 0.5, renderCell: (v: any) => {
+
+        return (
+          <Button
+            onClick={() => { clickHandler(v) }}
+            variant="text"
+            startIcon={<VisibilityIcon style={{ color: "white" }}
+            />}
+          ></Button >
+        )
+      }
+    }
+  ]
   return (
-    <div>CongNo</div>
+
+    <Box m="20px">
+      <Header title="Công nợ" subtitle="Danh sách công nợ" />
+      <Box>
+        <Typography width="50%" display="inline-block" variant="h2" fontWeight="bold" color={colors.greenAccent[400]} textAlign="center">
+          Công nợ chưa thu
+        </Typography>
+        <Typography width="50%" display="inline-block" variant="h2" fontWeight="bold" color={colors.greenAccent[400]} textAlign="center">
+          Công nợ đã thu
+        </Typography>
+        <Box
+          display="inline-block"
+          m="0 10px 0 0"
+          height="60vh"
+          width="calc(50% - 10px)"
+          sx={{
+            "& .MuiDataGrid-root": {
+              border: "none",
+            },
+            "& .MuiDataGrid-cell": {
+              borderBottom: "none",
+            },
+            "& .name-column--cell": {
+              color: colors.greenAccent[300],
+            },
+            "& .MuiDataGrid-columnHeaders": {
+              backgroundColor: colors.blueAccent[700],
+              borderBottom: "none",
+            },
+            "& .MuiDataGrid-virtualScroller": {
+              backgroundColor: colors.primary[400],
+            },
+            "& .MuiDataGrid-footerContainer": {
+              borderTop: "none",
+              backgroundColor: colors.blueAccent[700],
+            },
+            "& .MuiCheckbox-root": {
+              color: `${colors.greenAccent[200]} !important`,
+            },
+          }}>
+          <DataGrid
+            rows={mockDataReceivable.map((v) => { return { ...v, stt: ++countRow } })}
+            columns={columns}
+          />
+        </Box>
+        <Box
+          m="0 0 0 10px"
+          display="inline-block"
+          height="60vh"
+          width="calc(50% - 10px)"
+          sx={{
+            "& .MuiDataGrid-root": {
+              border: "none",
+            },
+            "& .MuiDataGrid-cell": {
+              borderBottom: "none",
+            },
+            "& .name-column--cell": {
+              color: colors.greenAccent[300],
+            },
+            "& .MuiDataGrid-columnHeaders": {
+              backgroundColor: colors.blueAccent[700],
+              borderBottom: "none",
+            },
+            "& .MuiDataGrid-virtualScroller": {
+              backgroundColor: colors.primary[400],
+            },
+            "& .MuiDataGrid-footerContainer": {
+              borderTop: "none",
+              backgroundColor: colors.blueAccent[700],
+            },
+            "& .MuiCheckbox-root": {
+              color: `${colors.greenAccent[200]} !important`,
+            },
+          }}>
+          <DataGrid
+            rows={mockDataCollected.map((v) => { return { ...v, stt: ++countRow1 } })}
+            columns={columns}
+          />
+        </Box>
+      </Box>
+      <BootstrapDialog
+        onClose={closeHandler}
+        aria-labelledby="customized-dialog-title"
+        open={open}
+      >
+        <BootstrapDialogTitle id="customized-dialog-title" onClose={closeHandler}>
+          Chi tiết công nợ
+        </BootstrapDialogTitle>
+        <DialogContent dividers>
+
+          {(ct => {
+            console.log(ct);
+            return (
+              <Typography gutterBottom>
+                hello
+              </Typography>
+            )
+          })(content)}
+        </DialogContent>
+        <DialogActions>
+          <Button autoFocus onClick={closeHandler}>
+            Save changes
+          </Button>
+        </DialogActions>
+      </BootstrapDialog>
+    </Box>
   )
 }
 
